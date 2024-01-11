f406798491fc451b3ed0c0748e82c6c7d5ec39b0
SUMIT DEY <sumitdey.cnd@gmail.com>
2024-01-11 23:33:57 +0530

    First commit for deployment

---
+++ app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from 'next'
+import { Urbanist } from 'next/font/google'
+import './globals.css'
+import Footer from '@/components/footer'
+import Navbar from '@/components/navbar'
+import ModalProvider from '@/providers/modal-provider'
+import ToastProvider from '@/providers/toast-provider'
+
+const font = Urbanist({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'RozaHarvest Store',
+  description: 'A rose selling online store',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ToastProvider />
+        <ModalProvider />
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
